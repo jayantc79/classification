f517de5a507e0a0c7958ecf89af1ae70bf6c88ce
Jayant Chaudhari <61195551+jayantc79@users.noreply.github.com>
2021-04-03 15:00:00 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("Image Classification with Google's Teachable Machine")
+st.header("Pneumothorax Classification Example")
+st.text("Upload a Image for image classification as pneumothorax or no-pneumothorax")
