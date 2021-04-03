5f2ecc64788347e6b5ff1f5fe930606c3b49b8f0
Jayant Chaudhari <61195551+jayantc79@users.noreply.github.com>
2021-04-03 15:06:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import streamlit as st
 st.title("Image Classification with Teachable Machine Learning")
 st.header("Normal X Ray Vs Pneumonia X Ray")
 st.text("Upload a X Ray to detect it is normal or has pneumonia")
+# file upload and handling logic
+uploaded_file = st.file_uploader("Choose a X Ray Image", type="jpeg")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert('RGB')
