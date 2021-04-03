5bc4ae59a36333b8792d2909dc010af6dbaa5fcc
Jayant Chaudhari <61195551+jayantc79@users.noreply.github.com>
2021-04-03 15:15:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("Image Classification with Teachable Machine Learning")
 st.header("Normal X Ray Vs Pneumonia X Ray")
 st.text("Upload a X Ray to detect it is normal or has pneumonia")
 # file upload and handling logic
-uploaded_file = st.file_uploader("Choose a X Ray Image", type="jpeg", "png")
+uploaded_file = st.file_uploader("Choose a X Ray Image", type='png')
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
 #image = Image.open(img_name).convert('RGB')
